3afe4ca1e558ce95aa44b458450e4533a853facf
LvLTroubleshooter <ismailliam144@gmail.com>
2024-11-17 19:37:04 +0800

    Empty setup

---
+++ src/main/java/com/myapp/money_planner/MoneyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.money_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/money_planner/MoneyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.money_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
